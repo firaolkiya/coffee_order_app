d61228f93b9f4eadc476f8f8b4dec197a2c34f9d
firaol <firaolkiya6@gmail.com>
2024-08-16 17:32:51 +0300

    clean architecture

--- lib/features/data/data_source/remote_data_source/remote_data_source.dart
+++ lib/features/data/data_source/remote_data_source/remote_data_source.dart
@@ -1,3 +1,39 @@
-class CoffeeRemoteDataSource{
+import 'package:coffee_shop/features/data/model/coffee_model.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/error/failure/failure.dart';
+import '../../../domain/entities/user_entity.dart';
+
+abstract class CoffeeRemoteDataSource{
+
+
+  Future<Either<Failure,CoffeeModel>>getCoffee(String id);
+
+  Future<Either<Failure,List<CoffeeModel>>> getALLCoffee();
+
+  Future<Either<Failure,bool>>addToFavorite(String id);
+  Future<Either<Failure,bool>>removeFromFavorite(String id);
+
+
+  Future<Either<Failure,List<CoffeeModel>>>getAllFavorite();
+
+  Future<Either<Failure,bool>>addToCart(String id);
+
+  Future<Either<Failure,bool>>requestCreateAccount(UserEntity user);
+
+  Future<Either<Failure,bool>>requestLogin(String email,String password);
+
+  Future<Either<Failure,List<CoffeeModel>>>gelAllFromCart();
+
+  Future<Either<Failure,List<CoffeeModel>>> getAllOrderedCoffee();
+
+  Future<Either<Failure,bool>>sendFeedback(String feedback);
+
+  Future<Either<Failure,String>> getAbout();
+
+  Future<Either<Failure,bool>> requestDownload();
+
+  Future<Either<Failure,String>> askHelp(String question);
+
 
 }
